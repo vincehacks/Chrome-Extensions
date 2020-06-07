5304e9d0748b846c429ebba24670aaf0fdd0bb14
Vince Chang <vincechangsf@gmail.com>
2020-06-06 20:18:06 -0700

    Budget Manager Extension Finished

--- README.md
+++ README.md
@@ -15,13 +15,15 @@ Building Chrome Extensions Here!
 - Description: This Google Chrome extension was built to accept spending,
   display total spending, limit and notify if reached and option to reset total
   and limit
-  ![Final Product](https://github.com/vincehacks/Chrome-Extensions/blob/master/vincehacks/FinalProduct.png)
+  ![Final Product](https://github.com/vincehacks/Chrome-Extensions/blob/master/budgetmanager/FinalProduct.png)
+  ![Final Product](https://github.com/vincehacks/Chrome-Extensions/blob/master/budgetmanager/FinalProduct1.png)
+  ![Final Product](https://github.com/vincehacks/Chrome-Extensions/blob/master/budgetmanager/FinalProduct2.png)
+  ![Final Product](https://github.com/vincehacks/Chrome-Extensions/blob/master/budgetmanager/FinalProduct3.png)
 
 #### HELLO WORLD CHROME EXTENSION
 
 - Description: This Google Chrome extension was built to demonstrate a simple
   Chrome extension that can be used along with Jquery to update text when typed
-  ![Final Product](https://github.com/vincehacks/Chrome-Extensions/blob/master/vincehacks/FinalProduct.png)
 
 #### SETTING UP MANIFEST.JSON
 
@@ -105,3 +107,26 @@ Chrome Web Store
 - Budget Manager
   - Set a Limit
   - Reset a Total
+
+#### BACKGROUND VS EVENT PAGES
+
+- 2 Types of Pages
+  1. **Background**
+  2. **Event**
+- Why: Code running in background
+- Background page **runs at all times**
+- Event page **runs only when required**
+- Background pages consume resources even when not required
+- Stick to event pages to avoid resources from always being used up
+- ContextMenus is when you can right click while on Chrome, you get some options
+  to help utilize your Chrome extension
+- ContextMenus are Event Pages not Background pages
+- For the ContextMenus to work, will need to utilize a different Chrome API that
+  will listen to the click event on the ContextMenus item
+
+#### BADGES
+
+- Can provide a badge on the Chrome icon
+  - Ex. ad blocker shows how many ads are blocked on a page without you having
+    to open and click on the Chrome extension
+- This is going to be using a listener and using a different Chrome API
---
+++ budgetmanager/eventpage.js
@@ -0,0 +1,60 @@
+// Created by: Vince Chang
+
+// This will create the SpendMoney option when selecting a value and right click
+
+var contextMenuItem = {
+  "id": "spendMoney",
+  "title": "SpendMoney", // When you right click on the web page
+  "contexts": ["selection"] // This is the context when this appears, so only
+  // when the user selects something on the web page (there are others to
+  // choose from so look at documentation)
+};
+chrome.contextMenus.create(contextMenuItem);
+
+
+function isInt(value) {
+  return !isNaN(value) &&
+    parseInt(Number(value)) == value && !isNaN(parseInt(value, 10));
+}
+
+// Listener for when a user clicks on the SpendMoney selection
+chrome.contextMenus.onClicked.addListener(function (clickData) {
+
+  // Check if what the user clicked on is the SpendMoney option & something was
+  // selected
+  if (clickData.menuItemId == "spendMoney" && clickData.selectionText) {
+    // Check if this was a integer that was selected by calling isInt
+    if (isInt(clickData.selectionText)) {
+      chrome.storage.sync.get(['total', 'limit'], function (budget) {
+        var newTotal = 0;
+        if (budget.total) {
+          newTotal += parseInt(budget.total);
+        }
+
+        // Add selected text to the total
+        newTotal += parseInt(clickData.selectionText);
+        chrome.storage.sync.set({ 'total': newTotal }, function () {
+          if (newTotal >= budget.limit) {
+            var notifOptions = {
+              type: 'basic', // There are different type of notifications to use
+              iconUrl: 'icon48.png',
+              title: 'Limit reached',
+              message: 'Uh oh! Looks like you\'ve reached your limit'
+            };
+            // The first parameter is ID for the notification, 2nd is the object
+            chrome.notifications.create('limitNotif', notifOptions);
+            chrome.notifications.clear('limitNotif');
+          }
+        });
+      });
+    }
+  }
+});
+
+
+// To update the Badge, changes will be all the variables that have changed in
+// the storage
+chrome.storage.onChanged.addListener(function (changes, storageName) {
+  // Set a badge
+  chrome.browserAction.setBadgeText({ "text": changes.total.newValue.toString() })
+})
